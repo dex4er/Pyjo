ee9dcf745ea5c08511eb54949ca5240050dd70a0
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-11-20 14:00:51 +0100

    ...

--- ref_weak.py
+++ ref_weak.py
@@ -2,18 +2,23 @@ import weakref
 
 
 class C:
-    ref = None
+    _ref = None
 
     def __del__(self):
         print "__del__ " + str(self)
 
+    def ref(self, value=None):
+        if value is None:
+            return self._ref
+        self._ref = weakref.proxy(value)
+        return self
 
 def test():
     a = C()
     b = C()
 
-    a.ref = weakref.proxy(b)
-    b.ref = weakref.proxy(a)
+    a.ref(b)
+    b.ref(a)
 
     print "{0}.ref = {1}".format(a, a.ref)
     print "{0}.ref = {1}".format(b, b.ref)
