ac4d17e96e41cb317d99ae59c79995a376f9c509
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-12-03 17:24:40 +0100

    Example: URL with querystring

--- examples/url.pl
+++ examples/url.pl
@@ -4,3 +4,5 @@ use Mojo::URL;
 
 my $url = Mojo::URL->new('https://github.com/dex4er/Pyjo');
 say $url->scheme('ssh+git')->userinfo('git')->path($url->path.'.git');
+
+say Mojo::URL->new('https://metacpan.org/search')->query(q => 'Mojo::URL', size => 20);
--- examples/url.py
+++ examples/url.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 
 from Pyjo.URL import *
+from Pyjo.Parameters import *
 
 url = Pyjo_URL('https://github.com/dex4er/Pyjo')
 print(str(url.set(scheme='ssh+git', userinfo='git', path=str(url.path)+'.git')))
+
+print(Pyjo_URL('http://metacpan.org/search').set(query={'q':'Mojo::URL', 'size':20}).to_string())
