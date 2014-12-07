84d565d543a0a5bfff800a40b4c15fb3b5d126fb
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-12-07 15:28:45 +0100

    Fixed benchmark

--- benchmarks/http_benchmark.py
+++ benchmarks/http_benchmark.py
@@ -59,7 +59,7 @@ def client_cb(loop, err, stream, i):
 for i in range(n):
     Pyjo.IOLoop.client(address=url.host,
                        port=(url.port or 80),
-                       cb=lambda loop, err, stream: client_cb(loop, err, stream, i))
+                       cb=(lambda i: lambda loop, err, stream: client_cb(loop, err, stream, i))(i))
 
 while True:
     Pyjo.IOLoop.start()
