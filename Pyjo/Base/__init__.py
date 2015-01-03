0ecec643e2a592c123881eaeefa53687fe907dc1
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-01-03 14:42:14 +0100

    docs

--- Pyjo/Base/__init__.py
+++ Pyjo/Base/__init__.py
@@ -1,7 +1,31 @@
-"""
-Pyjo.Base
-"""
+# -*- coding: utf-8 -*-
 
+"""
+Pyjo.Base - Minimal base class
+==============================
+::
+
+    import Pyjo.Base
+    from Pyjo.Util import lazy
+
+    class Cat(Pyjo.Base.object):
+        name = 'Nyan'
+        birds = 2
+        mice = 2
+
+    class Tiger(Cat):
+        friends = lazy(lambda self: Cat())
+        stripes = 42
+
+    mew = Cat.new(name='Longcat')
+    print(mew.mice)
+    print(mew.set(mice=3, birds=4).mice)
+
+    rawr = Tiger.new(stripes=23, mice=0)
+    print(rawr.tap(lambda self: self.friend.set(name='Tacgnol').mice))
+
+:mod:`Pyjo.Base` is a simple base class for :mod:`Pyjo` projects.
+"""
 
 from Pyjo.Util import lazy
 
@@ -19,10 +43,6 @@ class Pyjo_Base(object):
         self.set(**kwargs)
 
     @classmethod
-    def new(cls, *args, **kwargs):
-        return cls(*args, **kwargs)
-
-    @classmethod
     def attr(cls, attrs, default=None):
         if not isinstance(attrs, (list, tuple)):
             attrs = [attrs]
@@ -31,6 +51,10 @@ class Pyjo_Base(object):
             setattr(cls, attr, lazy(default, attr))
 
         return cls
+
+    @classmethod
+    def new(cls, *args, **kwargs):
+        return cls(*args, **kwargs)
 
     def set(self, **kwargs):
         for k, v in kwargs.items():
