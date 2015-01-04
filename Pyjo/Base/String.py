9b4d1fdf3c38dc992e70db41fbf04b7fcbe5f5a5
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-01-04 13:17:12 +0100

    Pyjo.Base.String more overloaded methods

--- Pyjo/Base/String.py
+++ Pyjo/Base/String.py
@@ -18,11 +18,73 @@ class Pyjo_Base_String(Pyjo.Base.object):
     def parse(self):
         pass
 
-    def __str__(self):
-        string = self.to_str()
-        if string is None:
-            string = 'None'
-        return string
+    def __add__(self, other):
+        return str(self) + other
+
+    def __bool__(self):
+        return bool(self.to_str())
+
+    def __bytes__(self):
+        return bytes(self.to_str(), 'utf-8')
+
+    def __complex__(self):
+        return complex(self.to_str())
+
+    def __contains__(self, other):
+        return other in str(self)
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __float__(self):
+        return float(self.to_str())
+
+    def __ge__(self, other):
+        return str(self) >= str(other)
+
+    def __gt__(self, other):
+        return str(self) > str(other)
+
+    def __hash__(self):
+        return hash(self.to_str())
+
+    def __hex__(self):
+        return hex(int(self.to_str()))
+
+    def __iadd__(self, other):
+        self.parse(str(self) + other)
+        return self
+
+    def __imul__(self, other):
+        self.parse(str(self) * other)
+        return self
+
+    def __int__(self):
+        return int(self.to_str())
+
+    def __le__(self, other):
+        return str(self) <= str(other)
+
+    def __long__(self):
+        return long(self.to_str())
+
+    def __lt__(self, other):
+        return str(self) < str(other)
+
+    def __mul__(self, other):
+        return str(self) * other
+
+    def __ne__(self, other):
+        return str(self) != str(other)
+
+    def __nonzero__(self):
+        return bool(self.to_str())
+
+    def __oct__(self):
+        return oct(int(self.to_str()))
+
+    def __radd__(self, other):
+        return other + str(self)
 
     def __repr__(self):
         if self.__module__ == '__main__':
@@ -30,52 +92,11 @@ class Pyjo_Base_String(Pyjo.Base.object):
         else:
             return "{0}.{1}('{2}')".format(self.__module__, self.__class__.__name__, str(self))
 
-    def __bytes__(self):
-        return bytes(self.to_str(), 'utf-8')
-
-    def __eq__(self, other):
-        return str(self) == str(other)
-
-    def __ne__(self, other):
-        return str(self) != str(other)
-
-    def __gt__(self, other):
-        return str(self) > str(other)
-
-    def __ge__(self, other):
-        return str(self) >= str(other)
-
-    def __lt__(self, other):
-        return str(self) < str(other)
-
-    def __le__(self, other):
-        return str(self) <= str(other)
-
-    def __nonzero__(self):
-        return bool(self.to_str())
-
-    def __bool__(self):
-        return bool(self.to_str())
-
-    def __add__(self, other):
-        return str(self) + other
-
-    def __radd__(self, other):
-        return other + str(self)
-
-    def __iadd__(self, other):
-        self.parse(str(self) + other)
-        return self
-
-    def __mul__(self, other):
-        return str(self) * other
-
-    def __imul__(self, other):
-        self.parse(str(self) * other)
-        return self
-
-    def __contains__(self, other):
-        return other in str(self)
+    def __str__(self):
+        string = self.to_str()
+        if string is None:
+            string = 'None'
+        return string
 
 
 new = Pyjo_Base_String.new
