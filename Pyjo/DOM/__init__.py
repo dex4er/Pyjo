6d7b5f6ed8a7474e559ee8ddf5f21372282a48b5
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-01-16 12:30:51 +0100

    Pyjo.DOM.find

--- Pyjo/DOM/__init__.py
+++ Pyjo/DOM/__init__.py
@@ -60,9 +60,40 @@ class Pyjo_DOM(Pyjo.Base.object, Pyjo.Mixin.String.object):
             self.parse(html)
 
     def at(self, pattern):
-        result = self._css().select_one(pattern)
+        """::
+
+            result = dom.at('div > p')
+
+        Find first element in DOM structure matching the CSS selector and return it as
+        a :mod:`Pyjo.DOM` object or return :class:`None` if none could be found. All selectors
+        from :mod:`Pyjo.DOM.CSS` are supported. ::
+
+            # Find first element with ``svg`` namespace definition
+            namespace = dom.at('[xmlns\:svg]')['xmlns:svg']
+        """
+        result = self._css.select_one(pattern)
         if result:
             return self._build(result, self.xml)
+
+    def find(self, pattern):
+        """::
+
+            collection = dom.find('div > p')
+
+        Find all elements in DOM structure matching the CSS selector and return a
+        :mod:`Pyjo.Collection` object containing these elements as :mod:`Pyjo.DOM` objects.
+        All selectors from :mod:`Pyjo.DOM.CSS` are supported. ::
+
+            # Find a specific element and extract information
+            _id = dom.find('div')[23]['id']
+
+            # Extract information from multiple elements
+            headers = dom.find('h1, h2, h3').map('text').each()
+
+            # Find elements with a class that contains dots
+            divs = dom.find('div.foo\.bar').each()
+        """
+        return self._collect(self._css.select(pattern))
 
     def parse(self, html):
         self.html.parse(html)
@@ -90,10 +121,11 @@ class Pyjo_DOM(Pyjo.Base.object, Pyjo.Mixin.String.object):
     def _build(self, tree, xml):
         return self.new().set(tree=tree, xml=xml)
 
-    def _collect(self, *args):
+    def _collect(self, results):
         xml = self.xml
-        return Pyjo.Collection.new(map(lambda a: self._build(a, xml), args))
+        return Pyjo.Collection.new(map(lambda a: self._build(a, xml), results))
 
+    @property
     def _css(self):
         return Pyjo.DOM.CSS.new(tree=self.tree)
 
