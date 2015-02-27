3c724ababadfa25bd0f806a8f66e8225b545c359
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-02-27 22:35:00 +0100

    Clean up

--- examples/singleton.pl
+++
@@ -1,7 +0,0 @@
-use v5.14;
-
-use Mojo::IOLoop;
-
-say Mojo::IOLoop::singleton;
-say Mojo::IOLoop->singleton;
-say Mojo::IOLoop->new->singleton;
--- examples/singleton.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-
-import Pyjo.IOLoop
-from Pyjo.IOLoop import Pyjo_IOLoop
-
-print(Pyjo.IOLoop.singleton)
-print(Pyjo_IOLoop.singleton)
-print(Pyjo_IOLoop().singleton)
