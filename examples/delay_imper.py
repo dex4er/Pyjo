29fe81b7b261d41caaf0d27c6ea1444731fc62ed
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-03-04 21:40:07 +0100

    Pyjo.IOLoop.Delay examples

--- examples/delay_imper.py
+++ examples/delay_imper.py
@@ -3,10 +3,6 @@ from __future__ import print_function
 import Pyjo.IOLoop
 
 
-delay = Pyjo.IOLoop.delay()
-
-
-@delay.step
 def step1(delay):
     print("Step 1")
     Pyjo.IOLoop.timer(delay.begin(), 2)
@@ -14,34 +10,36 @@ def step1(delay):
     print('Wait 2 seconds for step 2.')
 
 
-@delay.step
 def step2(delay):
     print("Step 2")
 
-    delay2 = Pyjo.IOLoop.delay()
-
-    @delay2.step
     def step2_1(delay2):
         print("Step 2.1")
         end = delay2.begin()
         Pyjo.IOLoop.timer(lambda loop: end('', 'OK'), 1)
         print('Wait 1 second for step 2.2.')
 
-    @delay2.step
     def step2_2(delay2, *args):
         print("Step 2.2 got {0}".format(args))
         Pyjo.IOLoop.timer(delay2.begin(), 3)
         print('Wait 3 seconds for step 3.')
 
-    delay2.step(delay.begin()).wait()
+    Pyjo.IOLoop.delay().steps(
+        step2_1,
+        step2_2,
+        delay.begin()
+    )
 
 
-@delay.step
 def step3(delay):
     print("Step 3")
     print('And done.')
 
 
-delay.wait()
+Pyjo.IOLoop.delay().steps(
+    step1,
+    step2,
+    step3
+).wait()
 
 print("END")
