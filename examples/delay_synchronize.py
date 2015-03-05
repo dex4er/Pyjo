92bc6b0327ae21bcac184c32fb625c9f4564018f
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-03-05 11:44:36 +0100

    Pyjo.IOLoop.Delay examples

---
+++ examples/delay_synchronize.py
@@ -0,0 +1,24 @@
+import Pyjo.IOLoop
+
+from Pyjo.Util import nonlocals
+
+
+# Synchronize multiple events
+delay = Pyjo.IOLoop.delay()
+
+@delay.step
+def step(delay):
+    print('BOOM!')
+
+for i in range(10):
+    end = delay.begin()
+
+    def timer_wrap(i):
+        def timer_cb(loop):
+            print(10 - i)
+            end()
+        return timer_cb
+
+    Pyjo.IOLoop.timer(timer_wrap(i), i)
+
+delay.wait()
