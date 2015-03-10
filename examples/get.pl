bd77b9fc39df579f9930a93805543c53e569e82a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-03-10 22:12:34 +0100

    Pyjo.UserAgent examples

--- examples/get.pl
+++ examples/get.pl
@@ -2,5 +2,8 @@ use v5.14;
 
 use Mojo::UserAgent;
 
-my $tx = Mojo::UserAgent->new->get($ARGV[0]);
+my $url = shift @ARGV or die 'get.pl url opts';
+my %opts = map { split /=/, $_, 2 } @ARGV;
+
+my $tx = Mojo::UserAgent->new(%opts)->get($url);
 say $tx->res->text;
--- examples/get.py
+++ examples/get.py
@@ -2,5 +2,12 @@ import Pyjo.UserAgent
 
 import sys
 
-tx = Pyjo.UserAgent.new().get(sys.argv[1])
+try:
+    url = sys.argv[1]
+except IndexError:
+    raise Exception('get.py url opts')
+
+opts = dict(map(lambda a: a.split('='), sys.argv[2:]))
+
+tx = Pyjo.UserAgent.new(**opts).get(url)
 print(tx.res.text)
