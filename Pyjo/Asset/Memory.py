1e7c652a748ea67938372a0f6a7af4e248efc4b5
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-03-19 17:13:49 +0100

    Pyjo.Asset.Memory.max_memory_size convert getenv's string to int

--- Pyjo/Asset/Memory.py
+++ Pyjo/Asset/Memory.py
@@ -40,7 +40,7 @@ Classes
 import Pyjo.Asset.File
 
 from Pyjo.Base import lazy
-from Pyjo.Util import getenv, notnone, spurt, steady_time
+from Pyjo.Util import convert, getenv, notnone, spurt, steady_time
 
 
 MTIME = steady_time()
@@ -62,7 +62,7 @@ class Pyjo_Asset_Memory(Pyjo.Asset.object):
     automatically upgrade to a :mod:`Pyjo.Asset.File` object.
     """
 
-    max_memory_size = lazy(lambda self: getenv('MOJO_MAX_MEMORY_SIZE', 262144))
+    max_memory_size = lazy(lambda self: convert(getenv('MOJO_MAX_MEMORY_SIZE'), int, 262144))
     """::
 
         size = asset_mem.max_memory_size
