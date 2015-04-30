c187dc7de2517347e8a9648fc405ffe1ca8b0523
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-04-30 19:19:18 +0200

    Pyjo.Transaction more methods

--- Pyjo/Transaction/__init__.py
+++ Pyjo/Transaction/__init__.py
@@ -3,26 +3,61 @@
 """
 Pyjo.Transaction - Transaction base class
 =========================================
-
 ::
 
     import Pyjo.Transaction
 
     class MyTransaction(Pyjo.Transaction.object):
 
-        def client_read(self):
+        def client_read(self, chunk):
             ...
 
         def client_write(self):
             ...
 
-        def server_read(self):
+        def server_read(self, chunk):
             ...
 
         def server_write(self):
             ...
 
 :mod:`Pyjo.Transaction` is an abstract base class for transactions.
+
+Events
+------
+
+:mod:`Pyjo.Transaction` inherits all events from :mod:`Pyjo.EventEmitter` and can
+emit the following new ones.
+
+connection
+~~~~~~~~~~
+::
+
+    @tx.on
+    def connection(tx, connection):
+        ...
+
+Emitted when a connection has been assigned to transaction.
+
+finish
+~~~~~~
+::
+
+    @tx.on
+    def finish(tx):
+        ...
+
+Emitted when transaction is finished.
+
+resume
+~~~~~~
+::
+
+    @tx.on
+    def resume(tx):
+        ...
+
+Emitted when transaction is resumed.
 
 Classes
 -------
@@ -33,7 +68,11 @@ import Pyjo.Message.Request
 import Pyjo.Message.Response
 
 from Pyjo.Base import lazy
-from Pyjo.Util import not_implemented
+from Pyjo.Regexp import r
+from Pyjo.Util import not_implemented, notnone
+
+
+re_x_forwarded_for = r(r'([^,\s]+)$')
 
 
 class Pyjo_Transaction(Pyjo.EventEmitter.object):
@@ -42,38 +81,123 @@ class Pyjo_Transaction(Pyjo.EventEmitter.object):
     :mod:`Pyjo.EventEmitter` and implements the following new ones.
     """
 
+    kept_alive = None
+    """::
+
+        kept_alive = tx.kept_alive
+        tx.kept_alive = True
+
+    Connection has been kept alive.
+    """
+
     local_address = None
+    """::
+
+        address = tx.local_address
+        tx.local_address = '127.0.0.1'
+
+    Local interface address.
+    """
+
     local_port = None
-    remote_address = None
+    """::
+
+        port = tx.local_port
+        tx.local_port = 8080
+
+    Local interface port.
+    """
+
+    original_remote_address = None
+    """::
+
+        address = tx.original_remote_address
+        tx.original_remote_address = '127.0.0.1'
+
+    Remote interface address.
+    """
+
     remote_port = None
+    """::
+
+        port = tx.remote_port
+        tx.remote_port = 8081
+
+    Remote interface port.
+    """
+
     req = lazy(lambda self: Pyjo.Message.Request.new())
+    """::
+
+        req = tx.req
+        tx.req = Pyjo.Message.Request.new()
+
+    HTTP request, defaults to a :mod:`Pyjo.Message.Request` object.
+    """
+
     res = lazy(lambda self: Pyjo.Message.Response.new())
+    """::
+
+        res = tx.res
+        tx.res = Pyjo.Message.Response.new()
+
+    HTTP response, defaults to a :mod:`Pyjo.Message.Response` object.
+    """
 
     _connection = None
     _state = None
 
     def client_close(self, close=False):
+        """::
+
+            tx.client_close()
+            tx.client_close(True)
+
+        Transaction closed client-side, no actual connection close is assumed by
+        default, used to implement user agents.
+        """
         # Premature connection close
         res = self.res.finish()
         if close and not res.code and not res.error:
-            res.error(message='Premature connection close')
+            res.set_error(message='Premature connection close')
 
         # 4xx/5xx
         elif res.is_status_class(400) or res.is_status_class(500):
-            res.error(message=res.message, code=res.code)
+            res.set_error(message=res.message, code=res.code)
 
         return self.server_close()
 
     @not_implemented
-    def client_read(self):
+    def client_read(self, chunk):
+        """::
+
+            tx.client_read(chunk)
+
+        Read data client-side, used to implement user agents. Meant to be overloaded in
+        a subclass.
+        """
         pass
 
     @not_implemented
     def client_write(self):
+        """::
+
+            chunk = tx.client_write
+
+        Write data client-side, used to implement user agents. Meant to be overloaded
+        in a subclass.
+        """
         pass
 
     @property
     def connection(self):
+        """::
+
+            cid = tx.connection
+            tx.connection = cid
+
+        Connection identifier.
+        """
         return self._connection
 
     @connection.setter
@@ -82,18 +206,151 @@ class Pyjo_Transaction(Pyjo.EventEmitter.object):
         self.emit('connection', value)
 
     @property
+    def error(self):
+        """::
+
+            err = tx.error
+
+        Get request or response error and return ``None`` if there is no error,
+        commonly used together with :attr:`success`. ::
+
+            # Longer version
+            err = tx.req.error or tx.res.error
+
+            # Check for different kinds of errors
+            err = tx.error
+            if err:
+                if err['code']:
+                    raise Exception("{code} response: {message}".format(**err))
+                else:
+                    raise Exception("Connection error: {message}".format(**err))
+        """
+        return self.req.error or self.res.error
+
+    @property
     def is_finished(self):
+        """::
+
+            bool = tx.is_finished
+
+        Check if transaction is finished.
+        """
         return self._state == 'finished'
 
     @property
+    def is_websocket(self):
+        """::
+
+            false = tx.is_websocket
+
+        False.
+        """
+        return False
+
+    @property
     def is_writing(self):
+        """::
+
+            boolean = tx.is_writing
+
+        Check if transaction is writing.
+        """
         if self._state is None:
             return True
         else:
             return self._state == 'write'
 
+    @property
+    def remote_address(self):
+        """::
+
+            address = tx.remote_address
+            tx.remote_address = '127.0.0.1'
+
+        Same as :attr:`original_remote_address` or the last value of the
+        ``X-Forwarded-For`` header if :attr:`req` has been performed through a reverse
+        proxy.
+        """
+        if not self.req.reverse_proxy:
+            return self.original_remote_address
+
+        # Reverse proxy
+        x_forwarded_for = notnone(self.req.headers.header('X-Forwarded-For'), '')
+        m = re_x_forwarded_for.search(x_forwarded_for)
+        if m:
+            return m.group(1)
+        else:
+            return self.original_remote_address
+
+    @remote_address.setter
+    def remote_address(self, value):
+        self.original_remote_address = value
+
+    def resume(self):
+        """::
+
+            tx = tx.resume
+
+        Resume transaction.
+        """
+        return self._state('write', 'resume')
+
     def server_close(self):
+        """::
+
+            tx.server_close
+
+        Transaction closed server-side, used to implement web servers.
+        """
         return self._state('finished', 'finish')
+
+    @not_implemented
+    def server_read(self, chunk):
+        """::
+
+            tx.server_read(chunk)
+
+        Read data server-side, used to implement web servers. Meant to be overloaded in
+        a subclass.
+        """
+        pass
+
+    @not_implemented
+    def server_write(self):
+        """::
+
+            chunk = tx.server_write()
+
+        Write data server-side, used to implement web servers. Meant to be overloaded
+        in a subclass.
+        """
+        pass
+
+    @property
+    def success(self):
+        """::
+
+            res = tx.success
+
+        Returns the :mod:`Pyjo.Message.Response` object from :attr:`res` if transaction was
+        successful or ``None`` otherwise. Connection and parser errors have only a
+        message in :attr:`error`, 400 and 500 responses also a code. ::
+
+            # Sensible exception handling
+            res = tx.success
+            if res:
+                print(res.body)
+            else:
+                err = tx.error
+                if err['code']:
+                    raise Exception("{code} response: {message}".format(**err))
+                else:
+                    raise Exception("Connection error: {message}".format(**err))
+        """
+        if self.error:
+            return None
+        else:
+            return self.res
 
     def _state(self, state, event):
         self._state = state
